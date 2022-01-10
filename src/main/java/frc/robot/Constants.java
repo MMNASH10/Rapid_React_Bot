325c82b13ba2b8522add0ef04d128b7a4cf4bf78
Morgan Nash <morganmaenash@gmail.com>
2022-01-10 15:57:24 -0500

    basic drive train

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,14 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int LEFT_FRONT = 1;
+    public static final int RIGHT_FRONT = 3;
+    public static final int LEFT_BACK = 2;
+    public static final int RIGHT_BACK = 4;
+
+    public static final int RIGHT_TRIGGER = 3;
+    public static final int LEFT_TRIGGER = 2;
+    public static final int XBOX_LEFT_X_AXIS = 0;
+    public static final double DRIVE_SPEED = 0;
+}
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,14 +4,63 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
+  private final WPI_TalonFX leftFront;
+  private final WPI_TalonFX rightFront;
+  private final WPI_TalonFX leftBack;
+  private final WPI_TalonFX rightBack;
+
+  private final DifferentialDrive difDrive;
+  
   /** Creates a new DriveTrain. */
-  public DriveTrain() {}
+  public DriveTrain() {
+    leftFront = new WPI_TalonFX(Constants.LEFT_FRONT);
+    rightFront = new WPI_TalonFX(Constants.RIGHT_FRONT);
+    leftBack = new WPI_TalonFX(Constants.LEFT_BACK);
+    rightBack = new WPI_TalonFX(Constants.RIGHT_BACK);
+
+    difDrive = new DifferentialDrive(leftFront, rightFront);
+
+    configDriveTrain();
+  }
+
+  public void drive(XboxController controller, double speed) {
+    difDrive.arcadeDrive(((controller.getRawAxis(Constants.RIGHT_TRIGGER))-(controller.getRawAxis(Constants.LEFT_TRIGGER)))*speed, controller.getRawAxis(Constants.XBOX_LEFT_X_AXIS)*-speed);
+  }
+
+  public void driveInverted(XboxController controller, double speed) {
+    difDrive.arcadeDrive(((controller.getRawAxis(Constants.RIGHT_TRIGGER))-(controller.getRawAxis(Constants.LEFT_TRIGGER)))*-speed, controller.getRawAxis(Constants.XBOX_LEFT_X_AXIS)*-speed);
+  }
+
+  public void stop() {
+    difDrive.stopMotor();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void configDriveTrain() {
+    /* factory default values */
+    leftFront.configFactoryDefault();
+    rightFront.configFactoryDefault();
+    leftBack.configFactoryDefault();
+    rightBack.configFactoryDefault();
+
+    /* set up followers*/
+    leftBack.follow(leftFront);
+    rightBack.follow(rightFront);
+    
+    
+    leftFront.setSensorPhase(true);
+    rightFront.setSensorPhase(true);
   }
 }
